e22cec5b328fae40fbef5520ce09a78c75b3038b
Simi <simiinemboj@gmail.com>
2022-10-05 21:28:36 +0000

    add a new section in settings.py databases

--- travellingfoodie/settings.py
+++ travellingfoodie/settings.py
@@ -85,6 +85,10 @@ WSGI_APPLICATION = 'travellingfoodie.wsgi.application'
 #     }
 # }
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
