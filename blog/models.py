64732b72e6b9ccb3f9a62d356d4bd9da42aebf55
Simi <simiinemboj@gmail.com>
2022-10-10 19:41:49 +0000

    Import ratings field

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from cloudinary.models import CloudinaryField
 from taggit.managers import TaggableManager
 from django_countries.fields import CountryField
 from django_summernote.fields import SummernoteTextField
+from djangoratings.fields import RatingField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
