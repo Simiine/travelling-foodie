5b6fa3099233e4b6cad4f84b36311afb94497c8a
Simi <simiinemboj@gmail.com>
2022-10-10 19:02:55 +0000

    Import country field for models

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 from taggit.managers import TaggableManager
+from django_countries.fields import CountryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
