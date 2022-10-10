8a184e0c830f4db6b0f45aa66c1c80acc066211d
Simi <simiinemboj@gmail.com>
2022-10-10 19:02:19 +0000

    import tag manager model

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from taggit.managers import TaggableManager
 
 STATUS = ((0, "Draft"), (1, "Published"))
+
+
