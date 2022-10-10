5888c0231ec0fb77b8977ac46573e2f50eed937d
Simi <simiinemboj@gmail.com>
2022-10-10 19:43:42 +0000

    Convert SQL table into a usable Django model

--- blog/models.py
+++ blog/models.py
@@ -8,5 +8,21 @@ from djangoratings.fields import RatingField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
-
+class Post(models.Model):
+    """
+    Model for Experiences
+    """
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    created_on = models.DateTimeField(auto_now_add=True)
+    featured_image = CloudinaryField('image', default='placeholder')
+    country = CountryField()
+    content = SummernoteTextField()
+    recipe = SummernoteTextField()
+    rating = RatingField(range=5) # 5 possible rating values, 1-5
+    tags = TaggableManager()
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='experience_likes', blank=True)
+    
 
