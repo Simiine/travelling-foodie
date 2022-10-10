843d18d850b45fe11e94c376d9fc12c43023eb6d
Simi <simiinemboj@gmail.com>
2022-10-10 19:57:08 +0000

    Change author related name to experience

--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,7 @@ class Experience(models.Model):
     """
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="experience")
     created_on = models.DateTimeField(auto_now_add=True)
     featured_image = CloudinaryField('image', default='placeholder')
     country = CountryField()
