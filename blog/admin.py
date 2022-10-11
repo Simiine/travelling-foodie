4e40159e345df8bcf8b879bae63d2fecde85009f
Simi <simiinemboj@gmail.com>
2022-10-11 19:49:08 +0000

    Remove country filter option from admin

--- blog/admin.py
+++ blog/admin.py
@@ -6,7 +6,7 @@ from django_summernote.admin import SummernoteModelAdmin
 class ExperienceAdmin(SummernoteModelAdmin):
     list_display = ('title', 'author', 'status', 'created_on', 'country', 'tags')
     search_fields = ['title', 'author']
-    list_filter = ('status', 'created_on', 'tags', 'country')
+    list_filter = ('status', 'created_on', 'tags')
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content', 'recipe')
 
