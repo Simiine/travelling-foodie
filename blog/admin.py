6737908a106a9f3ba783e20af83b380268b5fff5
Simi <simiinemboj@gmail.com>
2022-10-11 19:54:04 +0000

    Import comment model

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Experience
+from .models import Experience, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Experience)
