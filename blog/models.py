64b34ec38cdf1dcf565ca734a0dca58323ad6ca3
Simi <simiinemboj@gmail.com>
2022-10-11 20:20:28 +0000

    Return string for comment

--- blog/models.py
+++ blog/models.py
@@ -57,3 +57,9 @@ class Comment(models.Model):
         """
         ordering = ['created_on']
 
+    def __str__(self):
+        """
+        Return string representation
+        """
+        return f"Comment {self.body} by {self.name}"
+
