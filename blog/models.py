7f023085e1e490fa080e23fb7dd56ab1356da298
Simi <simiinemboj@gmail.com>
2022-10-11 20:18:18 +0000

    Order comments in ascending order

--- blog/models.py
+++ blog/models.py
@@ -51,3 +51,9 @@ class Comment(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
 
+    class Meta:
+        """
+        Order comments in ascending order
+        """
+        ordering = ['created_on']
+
