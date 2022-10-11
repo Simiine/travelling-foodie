3b4c5d713a8c8fc3202326bc7276026726c83b67
Simi <simiinemboj@gmail.com>
2022-10-11 19:46:12 +0000

    Add search fields to admin

--- blog/admin.py
+++ blog/admin.py
@@ -4,6 +4,7 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Experience)
 class ExperienceAdmin(SummernoteModelAdmin):
+    search_fields = ['title', 'author']
     list_filter = ('status', 'created_on', 'tags', 'country')
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content', 'recipe')
