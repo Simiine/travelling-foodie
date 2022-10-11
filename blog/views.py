83489c2373be1c041d482e0816db655834856c3b
Simi <simiinemboj@gmail.com>
2022-10-11 20:33:48 +0000

    Create class experience list

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Experience
+
+class ExperienceList(generic.ListView):
+    """
+    Creates Experience list
+    """
+    
