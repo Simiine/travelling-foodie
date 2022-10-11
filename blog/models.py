a9c5c89938f29fa5290f62e18fd3fc85bbf16a96
Simi <simiinemboj@gmail.com>
2022-10-11 20:06:29 +0000

    Add approved function to model

--- blog/models.py
+++ blog/models.py
@@ -49,4 +49,5 @@ class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_comments')
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
 
