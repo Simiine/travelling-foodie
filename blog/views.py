4344775fa18387a61bfff0ad02e020e72de2214b
Simi <simiinemboj@gmail.com>
2022-10-13 20:35:22 +0000

    Add experience detail class

--- blog/views.py
+++ blog/views.py
@@ -10,3 +10,25 @@ class ExperienceList(generic.ListView):
     queryset = Experience.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
+
+class ExperienceDetail(View):
+    """
+    Creates Experience detail view
+    """
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Experience.objects.filter(status=1)
+        experience = get_object_or_404(queryset, slug=slug)
+        comments = experience.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if experience.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "experience_detail.html",
+            {
+                "experience": experience,
+                "comments": comments,
+                "liked": liked
+            },
+        )
