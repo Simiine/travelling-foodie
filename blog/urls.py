a9b640cc6401d30c88fb30c0a62c5a4e9b0898ce
Simi <simiinemboj@gmail.com>
2022-10-13 21:00:56 +0000

    Wire up urls for experience detail

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.ExperienceList.as_view(), name='home'),
+    path('<slug:slug>/', views.ExperienceDetail.as_view(), name='experience_detail'),
 ]
