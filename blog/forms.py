a4b8b72c08e57a5a9f3f56e54961600f7c67c35d
Simi <simiinemboj@gmail.com>
2022-10-17 22:59:00 +0000

    Create add experience form

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,22 @@
-from .models import Comment
+from .models import Experience, Comment
+from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
 class CommentForm(forms.ModelForm):
+    """
+    Form for Comments
+    """
     class Meta:
         model = Comment
         fields = ('body',)
+
+class ExperienceForm(forms.ModelForm):
+    """
+    Form to Add Experience
+    """
+    class Meta:
+        model = Experience
+        fields = ('title', 'featured_image', 'country', 'content', 'recipe', 'tags',)
+    
+    def __init__(self, *args, **kwargs):
+        super(ExperienceForm, self).__init__(*args, **kwargs)
