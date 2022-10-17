ad4eef094680241e071c56e9ede056bc1eb2e05a
Simi <simiinemboj@gmail.com>
2022-10-17 22:58:23 +0000

    create add experience url path

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.ExperienceList.as_view(), name='home'),
     path('<slug:slug>/', views.ExperienceDetail.as_view(), name='experience_detail'),
     path('like/<slug:slug>', views.ExperienceLike.as_view(), name='experience_like'),
+    path('addexperience/', views.add_experience, name='addexperience'),
 ]
 
