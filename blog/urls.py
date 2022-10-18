20423f81685dff78221ef1bb51f96dccada524c1
Simi <simiinemboj@gmail.com>
2022-10-18 23:00:03 +0000

    Add urls for experience delete, edit and comment delete

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,13 @@
 from . import views
 from django.urls import path
+from .views import ExperienceEditView, ExperienceDeleteView
 
 urlpatterns = [
     path('', views.ExperienceList.as_view(), name='home'),
     path('<slug:slug>/', views.ExperienceDetail.as_view(), name='experience_detail'),
     path('like/<slug:slug>', views.ExperienceLike.as_view(), name='experience_like'),
     path('add_experience/', views.add_experience, name='add_experience'),
-
-   
+    path('experience_edit/<slug:slug>', views.ExperienceEditView.as_view(), name='experience_edit'),
+    path('experience_delete/<slug:slug>', views.ExperienceDeleteView.as_view(), name='experience_delete'),
+    path('delete_comment/<int:pk>', views.ExperienceDeleteComment.as_view(), name='delete_comment'),
 ]
-
