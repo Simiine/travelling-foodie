c738d33413465f67d820269f8de0ec6cff7e2564
Simi <simiinemboj@gmail.com>
2022-10-21 10:23:00 +0000

    remove tags

--- blog/admin.py
+++ blog/admin.py
@@ -4,9 +4,9 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Experience)
 class ExperienceAdmin(SummernoteModelAdmin):
-    list_display = ('title', 'author', 'status', 'created_on', 'country', 'tags')
+    list_display = ('title', 'author', 'status', 'created_on', 'country') #'tags')
     search_fields = ['title', 'author']
-    list_filter = ('status', 'created_on', 'tags')
+    list_filter = ('status', 'created_on') #'tags')
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content', 'recipe')
 
